82465982cfdcb89651acbabe0a2c3703ca7e2b28
Woodie-07 <40068306+Woodie-07@users.noreply.github.com>
2021-11-06 18:33:17 +0000

    Initial commit

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import find_packages, setup
+setup(
+    name='earnapp',
+    packages=find_packages(),
+    version='0.0.1',
+    description='A python library to interact with the EarnApp API',
+    author='Woodie',
+)
