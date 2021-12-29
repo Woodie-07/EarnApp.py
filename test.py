b53a38c09257bf9caa01f9beba48e92e10e3c1e3
Woodie-07 <40068306+Woodie-07@users.noreply.github.com>
2021-12-29 16:27:27 +0000

    Update to 0.0.2
    - Add support for unlinking devices
    - Add support for renaming devices
    - Add support for using proxies

--- test.py
+++ test.py
@@ -2,13 +2,17 @@
 
 from earnapp import earnapp
 
-token = "ENTER TOKEN HERE"
-deviceID = "ENTER TEST DEVICE ID"
+token = "OAUTH-REFRESH-TOKEN"
+deviceID = "DEVICE ID TO ADD"
+remDeviceID = "DEVICE ID TO REMOVE"
+proxy = {'https': 'socks5://user:pass@ip:port'}
 payoutEmail = "ENTER TEST PAYOUT EMAIL"
 
 print("Initializing user class")
 user = earnapp.User()
 print("Got user class")
+
+user.proxy = proxy 
 
 print("Attempting to log in with token " + token)
 loggedIn = user.login(token)
@@ -26,5 +30,7 @@ print("Payment methods: " + str(user.paymentMethods()))
 print("Transactions: " + str(user.transactions()))
 print("Attempting to link device ID " + deviceID)
 print(str(user.linkDevice(deviceID)))
+print("Attempting to remove device ID " + remDeviceID)
+print(str(user.unlinkDevice(remDeviceID)))
 print("Attempting to change payout email to " + payoutEmail)
 print(str(user.redeemDetails(payoutEmail)))
